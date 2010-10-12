a9f1b3c6b430d01ef593769e9b9d3ebea6cd9a1b
Evan Dorn <evan@lrdesign.com>
2010-10-12 15:27:56 -0700

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinjaDemo::Application.config.secret_token = '7e91a58a417ad3c80b00b40d9f462a6a622a9c9c2c19d3940a41c9767a3ec3c2eb71054896011d6bcc7de1509c1f8e877ec27ae22f0fdb61c2620d10c6eeec73'
