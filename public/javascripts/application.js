ca8e72e36a68293e232942513c2f9430eb8494a5
Evan <evan@lrdesign.com>
2010-10-13 16:54:27 -0700

    ajax links and forms work now

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,6 @@
 // This file is automatically included by javascript_include_tag :defaults
                 
 $.behavior({    
-  // '#ajax_new': $.ninja.make_ajax_link()
+  '#ajax_new': $.ninja.ajax_link(),
   'form#new_product': $.ninja.ajax_submission()
 })
