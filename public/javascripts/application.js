b85fef9de91977ff3a77f58d0cbf8682336bcb90
Evan <evan@lrdesign.com>
2010-10-14 10:59:12 -0700

    cleaned up the demo using new version of NS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,6 @@
 // This file is automatically included by javascript_include_tag :defaults
                 
 $.behavior({    
-  '#ajax_new': 				$.ninja.submits_as_ajax_link(),
-  'form#new_product': $.ninja.submits_as_ajax_form()
+  'a#ajax_me': 					$.ninja.submits_as_ajax_link(),
+  'form#new_product': 	$.ninja.submits_as_ajax_form()
 })
