c5c4ba96cdb465aa0b4b0abd679e54010ea3b9af
Evan <evan@lrdesign.com>
2010-10-13 22:34:44 -0700

    upgraded to use current version of NS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,6 @@
 // This file is automatically included by javascript_include_tag :defaults
                 
 $.behavior({    
-  '#ajax_new': 				$.ninja.ajax_link(),
-  'form#new_product': $.ninja.ajax_submission()
+  '#ajax_new': 				$.ninja.submits_as_ajax_link(),
+  'form#new_product': $.ninja.submits_as_ajax_form()
 })
