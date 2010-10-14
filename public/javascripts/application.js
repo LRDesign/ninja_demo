c430e33c99c76321ad6664094df2e8f27b2762e1
Evan <evan@lrdesign.com>
2010-10-14 16:22:34 -0700

    cleaned up a bunch of misnaming in deleteable_ and slow_ controllers

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,13 @@
 $.behavior({    
   'a#ajax_me': 					$.ninja.submits_as_ajax(),
-  'form#new_product': 	$.ninja.submits_as_ajax()
+  'form#new_product': 	$.ninja.submits_as_ajax({ 
+		busy_element: function(elem){ return $('#products table') } 
+	})
 })
 
                            
+
+// { busy_element: function(elem){ $('#products') } }
 
 
 
