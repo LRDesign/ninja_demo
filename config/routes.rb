95fa947ea7139bb04259c7f9cc7359469f996924
Evan <evan@lrdesign.com>
2010-10-13 17:27:58 -0700

    adde NS ui/CSS and created a slow controller to show spinner overlay

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 NinjaDemo::Application.routes.draw do
   resources :products
-
+  resources :slow_products
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
